67df9b9b20d43f23269df48ea62dbbed596c65ba
aashishpeepra <ironhack082327@gmail.com>
2020-08-29 20:17:44 +0530

    Added packagelock changes

---
+++ build/precache-manifest.fadf85d81084e455f1c37a11e46f4885.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9269ea87c8f0f934740f0d966166d7ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9fc3b3d8a49cd825ec1",
+    "url": "/static/css/4.9077ec15.chunk.css"
+  },
+  {
+    "revision": "f386a0727c132ae2efd9",
+    "url": "/static/css/main.866dceaf.chunk.css"
+  },
+  {
+    "revision": "faacd7b2550f1ff600a8",
+    "url": "/static/js/2.cd1efb2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd1efb2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cb032fbbebd32712b6",
+    "url": "/static/js/3.ad4a9216.chunk.js"
+  },
+  {
+    "revision": "d9fc3b3d8a49cd825ec1",
+    "url": "/static/js/4.e3686fee.chunk.js"
+  },
+  {
+    "revision": "5ccd7ff132041846ebb0",
+    "url": "/static/js/5.c261c581.chunk.js"
+  },
+  {
+    "revision": "f386a0727c132ae2efd9",
+    "url": "/static/js/main.a7b37cb9.chunk.js"
+  },
+  {
+    "revision": "7f4aa65bc5dccb6dd5a5",
+    "url": "/static/js/runtime-main.2a3e6a2d.js"
+  }
+]);
